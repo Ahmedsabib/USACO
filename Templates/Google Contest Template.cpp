0b0f36b0d5f421a157bc8fe2964eddaaefdb93fe
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2021-07-08 22:54:37 +0600

    Create Google Contest Template.cpp

---
+++ Templates/Google Contest Template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int T; cin >> T;
+    for(int testCase = 1; testCase <= T; ++testCase){
+        
+        // TODO: make sure all variable get reset.
+
+        cout << "Case #" << testCase << ": " <<  << '\n';
+    }
+    
+    return 0;
+}
