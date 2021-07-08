fba10f1f0eb8d5e726bf388cbf0555df857e8311
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2021-07-08 22:53:14 +0600

    Create Usaco Template.cpp

---
+++ Templates/Usaco Template.cpp
@@ -0,0 +1,22 @@
+/*
+ID: anchord
+PROG: 
+LANG: C++11
+*/
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    ifstream cin (" .in");
+    ofstream cout (" .out");
+
+    int T; cin >> T;
+    while(T--){
+
+        // TODO: make sure all variable get reset.    
+    }
+    
+    return 0;
+}
