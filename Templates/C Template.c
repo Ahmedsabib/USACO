ff39700f072b7c2af42560c3d0de5032824d44de
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2021-07-08 22:53:52 +0600

    Create C Template.c

---
+++ Templates/C Template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    
+    int T; scanf("%d", &T);
+    while(T--){
+        
+        // TODO: make sure all variable get reset.
+    }
+    
+    return 0;
+}
