362d3a3f2f63d52ab0f9d48d170d54787152b57d
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2021-07-14 00:53:43 +0600

    Update Own Macro Template.cpp

--- Templates/Own Macro Template.cpp
+++ Templates/Own Macro Template.cpp
@@ -33,40 +33,8 @@ const int MAX = 100001;
 const ll INF = 1e18;
 const ld PI = acos((ld)-1);
 
-template<class T> bool ckmin(T& a, const T& b) { 
-    return b < a ? a = b, 1 : 0; 
-}
- 
-template<class T> bool ckmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0; 
-}
- 
-template<class Fun>
-class y_combinator_result {
-    Fun fun_;
-public:
-    template<class T>
-    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
- 
-    template<class ...Args>
-    decltype(auto) operator()(Args &&...args) {
-        return fun_(std::ref(*this), std::forward<Args>(args)...);
-    }
-};
- 
-template<class Fun>
-decltype(auto) y_combinator(Fun &&fun) {
-    return  y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
-}
-
-void dbg_out() { cerr << endl; }
-
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
-    cerr << ' ' << H; dbg_out(T...); 
-}
-
 namespace io {
-    //template taken by bqi343
+    //io template taken by bqi343
     //INPUT
     void re(double& x) { string t; cin >> t; x = stod(t); } 
     void re(ld& x) { string t; cin >> t; x = stold(t); }
