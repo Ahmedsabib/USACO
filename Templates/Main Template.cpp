050349a2b3d1ef25a283abad319a50bf322cf798
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-07-01 13:59:06 +0600

    Update Main Template.cpp

--- Templates/Main Template.cpp
+++ Templates/Main Template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main() {
-    using namespace std;
-    ios_base::sync_with_stdio(false), cin.tie(nullptr);
-    
-    int T; cin >> T;
-    while(T--){
-        
-        // TODO: make sure all variable get reset.
-    }
-    
-    return 0;
+  ios_base::sync_with_stdio(0), cin.tie(0);
+
+  int tests;
+  cin >> tests;
+  for (int cases = 0; cases < tests; ++cases) {
+    // TODO: make sure all variable get reset.
+  }
+  return 0;
 }
